cee972c92c567c440ada8d2f0cff3623696fa7f5
Arenha22 <104299106+Arenha22@users.noreply.github.com>
2022-04-25 00:32:34 +0700

    Add files via upload

---
+++ UTS/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+
+namespace UTS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ourWindow = new NativeWindowSettings()
+			{
+				Size = new Vector2i(600, 600),
+				Title = "Project UTS"
+			};
+
+			using (var window = new Window(GameWindowSettings.Default, ourWindow))
+			{
+				window.Run();
+			}
+		}
+	}
+}
